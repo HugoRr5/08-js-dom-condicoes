ee1eed9958654cca599640aa80627249f9f77db3
HugoRr5 <132697347+HugoRr5@users.noreply.github.com>
2023-07-11 22:22:50 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -36,34 +36,34 @@ function carregar() {
       var genero = '';
       var img = document.createElement('img');
       if (fsex[1].checked) {
-        genero = 'mas';
+        genero = 'homem calvo';
         if (idade >= 0 && idade < 15) {
           // crianca
-          img.setAttribute('src', 'Criança-M.jpg');
+          img.setAttribute('src', 'foto-de-menino.png');
         } else if (idade < 30) {
           // jovem
-          img.setAttribute('src', 'Jovem-M.jpg');
+          img.setAttribute('src', 'foto-de-adolecenteM.png');
         } else if (idade < 50) {
           // adulto
-          img.setAttribute('src', 'Adulto-M.jpg');
+          img.setAttribute('src', 'foto-de-adulto.png');
         } else {
           // idoso
-          img.setAttribute('src', 'Idoso-M.jpg');
+          img.setAttribute('src', 'foto-de-velho.png');
         }
       } else if (fsex[2].checked) {
-        genero = 'fem';
+        genero = 'mulher';
         if (idade >= 0 && idade < 10) {
           // crianca
-          img.setAttribute('src', 'Criança-F.jpg');
+          img.setAttribute('src', 'foto-de-menina.png');
         } else if (idade < 21) {
           // jovem
-          img.setAttribute('src', 'Jovem-F.jpg');
+          img.setAttribute('src', 'foto-de-adolecenteF.png');
         } else if (idade < 50) {
           // adulto
-          img.setAttribute('src', 'Adulto-F.jpg');
+          img.setAttribute('src', 'foto-de-mulher.png');
         } else {
           // idoso
-          img.setAttribute('src', 'Idosa-F.jpg');
+          img.setAttribute('src', 'foto-de-velha.png');
         }
       }
       res.style.textAlign = 'center';
